9f295c05b9cce16bf7ebdb5133c47640497b81da
Brian Bycynski <high5bri@hotmail.com>
2022-02-03 02:34:55 -0500

    Init Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+//
+//
+
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+//
+//
+
+import '../styles/Navbar.css';
+import logo from '../images/airbnb-logo.png';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={logo} className='nav--logo' alt='logo' />
+    </nav>
+  );
+}
